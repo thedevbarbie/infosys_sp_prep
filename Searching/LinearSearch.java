a98df9d730d284bccb66858b047afb1bc9f73b87
thedevbarbie <thisgirlisabarbie@gmail.com>
2025-01-25 23:57:06 +0530

    Linear Search

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,25 @@
+package Searching;
+
+public class LinearSearch {
+    static int search(int[] a, int n, int x) {
+        for(int i=0; i<n; i++) {
+            if(a[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int a[] = {34, 5, 2, 33};
+        int n = a.length;
+        int x = 34;
+
+        int search_index = search(a, n, x);
+        if(search_index == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + search_index);
+        }
+    }
+    
+}
